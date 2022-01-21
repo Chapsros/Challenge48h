545824080f18a591b19fabea15aebb239d5b070a
Buges Killian <killian.buges@ynov.com>
2022-01-21 09:09:04 +0100

    update

--- Main.sol
+++ Main.sol
@@ -1,1 +1,19 @@
 pragma solidity >=0.7.0 <0.9.0;
+
+contract votingSystem{
+
+    struct vote{
+        address voterAddress;
+        bool decision;
+    }
+    
+    uint voteCount = 0;
+
+    enum state{created,voting,ended}
+    modifier inState(State _state) {
+		    require(state == _state);
+		    _;
+	    }
+
+
+}
